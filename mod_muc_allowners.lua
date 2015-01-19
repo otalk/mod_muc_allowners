e7cd694b50861eeb1584a669ab416e7ade53d7f3
Lance Stout <lancestout@gmail.com>
2015-01-19 11:20:40 -0800

    Initial commit

---
+++ mod_muc_allowners.lua
@@ -0,0 +1,13 @@
+-- Make everyone joining the room an owner
+module:hook("muc-occupant-joined", function (event)
+    local room, occupant = event.room, event.occupant;
+    room:set_affiliation(true, occupant.bare_jid, "owner");
+end, 2);
+
+-- Remove owner affiliation upon leaving the room to keep things tidy
+module:hook("muc-occupant-left", function (event)
+    local room, occupant = event.room, event.occupant;
+    room:set_affiliation(true, occupant.bare_jid, nil);
+end, 2);
+
+module:log("info", "mod_muc_allowners loaded");
