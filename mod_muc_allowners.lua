191e41976817c09c3b425f2ff2e7c3457dbb2e91
Lance Stout <lancestout@gmail.com>
2015-01-26 12:48:27 -0800

    Fix exception condition with using true

--- mod_muc_allowners.lua
+++ mod_muc_allowners.lua
@@ -1,13 +1,13 @@
 -- Make everyone joining the room an owner
 module:hook("muc-occupant-joined", function (event)
     local room, occupant = event.room, event.occupant;
-    room:set_affiliation(true, occupant.bare_jid, "owner");
+    room:set_affiliation(occupant.bare_jid, occupant.bare_jid, "owner");
 end, 2);
 
 -- Remove owner affiliation upon leaving the room to keep things tidy
 module:hook("muc-occupant-left", function (event)
     local room, occupant = event.room, event.occupant;
-    room:set_affiliation(true, occupant.bare_jid, nil);
+    room:set_affiliation(occupant.bare_jid, occupant.bare_jid, nil);
 end, 2);
 
 module:log("info", "mod_muc_allowners loaded");
