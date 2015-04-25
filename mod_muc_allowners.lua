2f3b81025e6dc4543f32a0d946d27da4394491eb
Lance Stout <lancestout@gmail.com>
2015-04-25 11:40:30 -0700

    Do this the super simpler way

--- mod_muc_allowners.lua
+++ mod_muc_allowners.lua
@@ -1,13 +1,7 @@
--- Make everyone joining the room an owner
-module:hook("muc-occupant-joined", function (event)
-    local room, occupant = event.room, event.occupant;
-    room:set_affiliation(occupant.bare_jid, occupant.bare_jid, "owner");
-end, 2);
+local muc_service = module:depends("muc");
+local room_mt = muc_service.room_mt;
 
--- Remove owner affiliation upon leaving the room to keep things tidy
-module:hook("muc-occupant-left", function (event)
-    local room, occupant = event.room, event.occupant;
-    room:set_affiliation(occupant.bare_jid, occupant.bare_jid, nil);
-end, 2);
 
-module:log("info", "mod_muc_allowners loaded");
+room_mt.get_affiliation = function (room, jid)
+    return "owner";
+end
